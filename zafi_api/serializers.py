35e82842da793b297e2632c5b805834dd64c6978
Pepice23 <mojo12984@gmail.com>
2022-02-25 10:34:37 +0100

    Create serializers.py

---
+++ zafi_api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Character
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = (
+            "character_name",
+            "character_gender",
+            "character_faction",
+            "character_race",
+            "character_class",
+        )
