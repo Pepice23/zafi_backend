87527eda87eefd0637ee23708bddc92805d38ab7
Pepice23 <mojo12984@gmail.com>
2022-02-25 10:34:32 +0100

    Update models.py

--- zafi_api/models.py
+++ zafi_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    character_name = models.CharField(
+        max_length=32, primary_key=True, unique=True, verbose_name="Character name:"
+    )
+    character_gender = models.CharField(max_length=6, verbose_name="Character gender:")
+    character_faction = models.CharField(max_length=8, verbose_name="Faction")
+    character_race = models.CharField(max_length=32, verbose_name="Race")
+    character_class = models.CharField(max_length=32, verbose_name="Player Class")
+
+    def __str__(self):
+        return self.character_name
