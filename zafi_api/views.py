b57f0e3c9194dfae5603b01b144e9d0bd4419010
Pepice23 <mojo12984@gmail.com>
2022-02-25 11:56:45 +0100

    added namecheck view

--- zafi_api/views.py
+++ zafi_api/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .bad_words import check_text
 from .models import Character
 from .serializers import CharacterSerializer
 
@@ -11,3 +15,18 @@ class ListCharacter(generics.ListAPIView):
 class DetailCharacter(generics.RetrieveAPIView):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
+
+
+class NameCheck(APIView):
+    def get(self, request, name: str):
+        queryset = Character.objects.all()
+        if name is not None:
+            check_name = check_text(name)
+            if check_name == name:
+                queryset = queryset.filter(character_name=name.capitalize())
+                if len(queryset) > 0:
+                    return Response(
+                        f"There is already a character named: {name} in the database"
+                    )
+            else:
+                return Response(check_name)
