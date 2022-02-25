e7516d319a53083f595ccc47db4b12e58e80c39b
Pepice23 <mojo12984@gmail.com>
2022-02-25 11:56:32 +0100

    added namecheck url

--- zafi_api/urls.py
+++ zafi_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListCharacter, DetailCharacter
+from .views import ListCharacter, DetailCharacter, NameCheck
 
 urlpatterns = [
+    path("namecheck/<str:name>", NameCheck.as_view(), name="name_check"),
     path("<str:pk>/", DetailCharacter.as_view(), name="character_detail"),
     path("", ListCharacter.as_view(), name="character_list"),
 ]
