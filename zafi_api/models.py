c5fa0e3f45a7770802f569e31c01a2ef968b55c7
Pepice23 <mojo12984@gmail.com>
2022-06-16 12:59:32 +0200

    forgot to change verbose name to do as well

--- zafi_api/models.py
+++ zafi_api/models.py
@@ -24,14 +24,12 @@ class OfficeUpgrade(models.Model):
     add_money_per_sec = models.IntegerField(
         verbose_name="Add money per second:", default=0
     )
-    do_work_per_sec = models.IntegerField(
-        verbose_name="Add work per second:", default=0
-    )
+    do_work_per_sec = models.IntegerField(verbose_name="Do work per second:", default=0)
     add_money_per_click = models.IntegerField(
         verbose_name="Add money per click:", default=0
     )
     do_work_per_click = models.IntegerField(
-        verbose_name="Add work per click:", default=0
+        verbose_name="Do work per click:", default=0
     )
     office_upgrade_timer = models.IntegerField(verbose_name="Timer:", default=0)
     office_upgrade_name = models.CharField(max_length=32, verbose_name="Upgrade name:")
