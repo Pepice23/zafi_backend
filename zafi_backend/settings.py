4db56129290434328f78fa6e8778d0698ec8da35
Pepice23 <mojo12984@gmail.com>
2022-06-27 10:58:51 +0200

    added authentication

--- zafi_backend/settings.py
+++ zafi_backend/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # 3rd Party
     "rest_framework",
     "corsheaders",
+    "rest_framework.authtoken",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
     # Local
     "zafi_api.apps.ZafiApiConfig",
 ]
@@ -47,6 +54,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
@@ -85,6 +96,9 @@ TEMPLATES = [
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+SITE_ID = 1
 
 WSGI_APPLICATION = "zafi_backend.wsgi.application"
 
--- zafi_backend/urls.py
+++ zafi_backend/urls.py
@@ -16,4 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("zafi_api.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("zafi_api.urls")),
+    path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
+    path(
+        "api/dj-rest-auth/registration/",
+        include("dj_rest_auth.registration.urls"),
+    ),
+]
